38306d4b441087cf0e4fa179e7aefb0c948ecd29
wathanyuphosri0-ship-it <wathanyuphosri0@gmail.com>
2026-08-06 02:29:02 +0700

    EnemyDmgDealer

---
+++ Assets/NAMO/Script/EnemyDamageDealer.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class EnemyDamageDealer : MonoBehaviour
+{
+    [SerializeField] private int damageAmount = 1;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerHealth playerHealth = collision.gameObject.GetComponent<PlayerHealth>();
+            if (playerHealth != null)
+            {
+                playerHealth.TakeDamage(damageAmount, transform.position);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
+            if (playerHealth != null)
+            {
+                playerHealth.TakeDamage(damageAmount, transform.position);
+            }
+        }
+    }
+}
